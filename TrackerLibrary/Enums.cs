c0fc6cb0fa00241c5dcb19b8289ddf4f1c6b53ca
etnAsiemanym <ante09@gmail.com>
2023-02-07 22:25:09 +0100

    adding to file/sql input

---
+++ TrackerLibrary/Enums.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TrackerLibrary
+{
+    public enum DatabaseType
+    {
+        Sql,
+        TextFile
+    }
+}
--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -10,22 +10,22 @@ namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get;private set; } = new List<IDataConnection>();
+        public static IDataConnection Connection { get;private set; }
 
-        public static void InitializeConnections(bool database, bool textFiles)
+        public static void InitializeConnections(DatabaseType db)
         {
-            if (database) 
+            if (db == DatabaseType.Sql) 
             {
                 // TODO - set up SQL connector properly
                 SqlConnector sql = new SqlConnector();
-                Connections.Add(sql);
+                Connection = sql;
             }
 
-            if (textFiles)
+            else if (db == DatabaseType.TextFile)
             {
                 // TODO - create text connection
                 TextConnector text = new TextConnector();
-                Connections.Add(text);
+                Connection = text;
             }
         }
         public static string CnnString(string name)
